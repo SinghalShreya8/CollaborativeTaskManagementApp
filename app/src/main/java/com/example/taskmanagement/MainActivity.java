b9350f851439a777d6e349697062ee9a7bd79d35
Himanichauhann <46685911+Himanichauhann@users.noreply.github.com>
2020-06-14 16:27:16 +0530

    Sign In conflicts sorted

--- app/src/main/java/com/example/taskmanagement/MainActivity.java
+++ app/src/main/java/com/example/taskmanagement/MainActivity.java
@@ -26,6 +26,7 @@ import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.taskmanagement.ui.main.SectionsPagerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -33,6 +34,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
     DocumentReference documentReference;
 
     @Override
@@ -85,11 +87,17 @@ public class MainActivity extends AppCompatActivity {
                         build();
 
                 GoogleSignInClient googleSignInClient= GoogleSignIn.getClient(this,gso);
-                googleSignInClient.signOut();
-                Intent i2 = new Intent(MainActivity.this,signIn.class);
-                i2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                Toast.makeText(MainActivity.this,"Logged out successfully", Toast.LENGTH_SHORT).show();
-                startActivity(i2);
+                googleSignInClient.signOut().addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        mAuth.signOut();
+                        Intent i2 = new Intent(MainActivity.this,signIn.class);
+                        i2.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        Toast.makeText(MainActivity.this,"Logged out successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(i2);
+                    }
+                });
+
 
                 return true;
             case R.id.action_completed_tasks:
--- app/src/main/java/com/example/taskmanagement/SplashScreen.java
+++ app/src/main/java/com/example/taskmanagement/SplashScreen.java
@@ -2,20 +2,31 @@ package com.example.taskmanagement;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.WindowManager;
 
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class SplashScreen extends AppCompatActivity{
     private static int SPLASH_SCREEN_TIME_OUT=4000;
+    private static String TAG = "splashscreen";
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    FirebaseUser user = mAuth.getCurrentUser() ;
+    String id = mAuth.getUid();
    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         //This method is used so that your splash activity
         //can cover the entire screen.
 
@@ -25,11 +36,18 @@ public class SplashScreen extends AppCompatActivity{
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i=new Intent(SplashScreen.this,signIn.class);
+                if(user !=null){
+                    Log.e(TAG, "run: user found");
+                    Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Log.e(TAG, "run: user not found");
+                    Intent i=new Intent(SplashScreen.this,signIn.class);
+                    startActivity(i);
+                    finish();
+                }
 
-                startActivity(i);
-
-                finish();
                }
         }, SPLASH_SCREEN_TIME_OUT);
     }
